b5970b44d8d13c0d75f97aa12ec1c57d0d4cf52c
daovanduc <duc07092000@gmail.com>
2020-06-23 16:14:51 +0700

    form login and validate

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
+function checkboxRobot(c: AbstractControl) {
+  const v = c.value;
+  return (v.checkbox === true) ? null : { isRobot: true };
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public userLogin: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.userLogin = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      checkbox: [false, { validators: checkboxRobot }]
+    })
+  }
+
+  login() {
+    console.log(this.userLogin.value)
   }
 
 }
